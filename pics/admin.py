21051cf8ae6655d7d25d5e65e0ed3609125fb0d4
devseme <ian.ochenge@student.moringaschool.com>
2021-11-28 00:39:50 +0300

    worked on the location for photos

--- pics/admin.py
+++ pics/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import photos,Category
+from .models import photos,Category,Location
 
 
 admin.site.register(photos)
 admin.site.register(Category)
+admin.site.register(Location)
--- pics/models.py
+++ pics/models.py
@@ -7,7 +7,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class Location(models.Model):
+    position = models.CharField(max_length =30)
+    
+
+    def __str__(self):
+        return self.position
 class photos(models.Model):
     # title field
     title = models.CharField(max_length=100)
@@ -16,16 +22,18 @@ class photos(models.Model):
     name = models.CharField(max_length =30)
     description = models.CharField(max_length =50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
 
     def save_photos(self):
         self.save()
 
 
     # update photos
-    def update_photos(self, name, description,category):
+    def update_photos(self, name, description,category,location):
         self.name = name
         self.description = description
         self.category = category
+        self.location =location
         self.save()
 
     # get all photos
