6d051d2d25dfb75073126211b1db09332a56bc1f
devseme <ian.ochenge@student.moringaschool.com>
2021-11-29 00:37:35 +0300

    added fontawesome and started on tests

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import photos,Category,Location
+import datetime as dt
 
 # Create your tests here.
+
+#testing the photos class
+class Testphotos(TestCase):
+    # Set up method
+    def setUp(self):
+        self.category= Category(name = 'Travel')
+        self.category.save()
+
+        self.location
